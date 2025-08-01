8deab64493351a8ffc95c532524cab24fec33a17
Daniele Rondina <geaaru@macaronios.org>
2025-08-01 13:14:44 +0000

    Bump www-client/firefox-bin-141.0

--- www-client/firefox-bin/files/local-settings.js
+++ www-client/firefox-bin/files/local-settings.js
@@ -1,2 +1,2 @@
-pref("general.config.filename", "all-gentoo.js");
+pref("general.config.filename", "all-macaroni.js");
 pref("general.config.obscure_value", 0); // use this to disable the byte-shift
